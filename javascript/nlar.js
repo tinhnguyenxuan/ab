344c41c5de51b6e0036c8eb6e36e84a9d7406dd1
tinhnguyenxuan <101899547+tinhnguyenxuan@users.noreply.github.com>
2022-03-19 06:08:54 +0700

    Add files via upload

---
+++ javascript/nlar.js
@@ -0,0 +1,42 @@
+var app = angular.module('myApp',[]);
+app.controller('myController', function($scope) {
+	$scope.abm = "About Me";
+	$scope.updateInfo = "Updated 20 minuted ago";
+	$scope.verify = "Verify";
+	$scope.des = "Description about me !";
+	$scope.cv = "Công việc";
+	$scope.job1 = "xnxx.com";
+	$scope.job2 = "clgt.net";
+	$scope.contact = "Thông tin liên lạc";
+	$scope.contact1 = "";
+	$scope.contact2 = "";
+	$scope.contactmi = " LIÊN HỆ TÔI";
+	$scope.ifyou = "";
+	$scope.modal1 = "Facebook";
+	$scope.modal2 = "Zalo";
+	$scope.modal3 = "SnapChat";
+	$scope.modal4 = "Email";
+	$scope.modal5 = "Sms";
+	$scope.linkfb = "https://www.facebook.com/nguyen.xuan.tinh.2007";
+	$scope.linkzl = "";
+	$scope.linksc = "";
+	$scope.linkgm = "";
+	$scope.phonenumber = "0852271064 - Vinaphone";
+	$scope.email = "";
+	$scope.title2 = "My Story";
+	$scope.contentStory1 = "l";
+	$scope.contentStory2 = "";
+	$scope.storytitle1 = "";
+	$scope.storytitle2 = "";
+	$scope.nothinglikeus = "Thank for watching";
+	$scope.thankall = "";
+	$scope.au = "Lmint";
+	$scope.phone = false;
+	$scope.reverse = function () {
+		$scope.phone= !$scope.phone;
+	}
+	$scope.gmail = false;
+	$scope.reverse2 = function () {
+		$scope.gmail= !$scope.gmail;
+	};
+});
